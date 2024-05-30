5ebdb6d130b69018d308546dc26f3de12436adfb
thinh205171 <thinhprodtrai@gmail.com>
2024-05-30 23:51:33 +0700

    fix port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 import assemblyai as aai
 
@@ -30,4 +31,5 @@ def speechToTexApi():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
